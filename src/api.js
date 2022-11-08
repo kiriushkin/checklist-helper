adee129c3aadc7fd319d69a74c8116c125f489f9
Kiriushkin Dmitrii <kiriushkindmitrii@gmail.com>
2022-11-08 16:20:01 +0300

    Remove .github

--- src/api.js
+++ src/api.js
@@ -260,6 +260,8 @@ const checkLineCorrectness = async (line, lineNum, key) => {
         line: lineNum,
         file: key,
       });
+
+    if (key === 'CSS') console.log(result[key][rule.key].errors);
   });
 };
 
